1eb0cc607742533acab822b2aa2adc8c7f556c59
Remington Stone <remstone7@gmail.com>
2026-04-25 12:20:28 -0600

    more testing/examples

--- examples/nestjs-basic/src/reports/reports.module.ts
+++ examples/nestjs-basic/src/reports/reports.module.ts
@@ -1,9 +1,8 @@
 import { Module } from "@nestjs/common";
 
 import { ReportsProcessor } from "./reports.processor.js";
-import { ReportsService } from "./reports.service.js";
 
 @Module({
-  providers: [ReportsService, ReportsProcessor],
+  providers: [ReportsProcessor],
 })
 export class ReportsModule {}
